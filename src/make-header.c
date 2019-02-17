6a618cb934f71179a235626e0f5137b77ba96ff5
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2019-02-17 03:28:42 +0000

    Visually report errors

--- src/make-header.c
+++ src/make-header.c
@@ -4,13 +4,14 @@
 int make_header(int status, char *buf, int buflen)
 {
 	const char *reason;
+	int ok = 0;
 	switch (status) {
-	case 200: reason = "OK"; break;
+	case 200: reason = "OK"; ok = 1; break;
 	case 400: reason = "Bad Request"; break;
 	case 404: reason = "Not Found"; break;
 	case 501: reason = "Not Implemented"; break;
-	default: reason = "???"; break;
+	default: reason = "???"; ok = 1; break;
 	}
-	return snprintf(buf, buflen, "HTTP/1.1 %d %s\r\n\r\n",
-		status, reason);
+	return snprintf(buf, buflen, "HTTP/1.1 %d %s\r\n\r\n%s",
+		status, reason, ok ? "" : reason);
 }
